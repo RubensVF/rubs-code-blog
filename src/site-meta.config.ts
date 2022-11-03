8cd2843b3bc37ea67216168a895af8c2b9fc6555
unknown <rubenvelazquez244@gmail.com>
2022-11-02 22:49:37 -0600

    Primeros pasos

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,8 +1,8 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
+  title: "Rubs Code Blog",
+  description: "Blog acerca de desarollo de software",
+  lang: "es-GB",
+  ogLocale: "es_GB",
   githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
